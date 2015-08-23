a03baf4a49f65695ce9843953856c37d29395fd2
Maya Schuster <mayasc@wix.com>
2015-08-23 17:50:40 +0300

    rt ex 5

--- src/hello.js
+++ src/hello.js
@@ -3,6 +3,22 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
 
     return React.createClass({
         displayName: 'Hello',
+
+        mixins: [React.addons.LinkedStateMixin],
+
+        getInitialState: function () {
+            var fields = ['first'];
+            return {fields: fields, newFieldName: ''};
+        },
+
+        handleClick: function () {
+            this.setState({fields: this.state.fields.concat([this.state.newFieldName])});
+        },
+
+        handleFieldChange: function (e) {
+            window.console.log(e.target.value);
+        },
+
         render: template
     });
 });
