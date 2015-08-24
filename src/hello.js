e21ee1c87749a4f9b771288f2bdc76946cae14c3
Maya Schuster <mayasc@wix.com>
2015-08-24 15:24:21 +0300

    rt ex 7

--- src/hello.js
+++ src/hello.js
@@ -7,22 +7,11 @@ define(['react', 'lodash', './hello.rt'], function (React, _, template) {
         mixins: [React.addons.LinkedStateMixin],
 
         getInitialState: function () {
-            return {location: {long: 1, lat: 1}};
+            return {};
         },
 
-        handleChange: function (prop, newValue) {
-            var newState = _.cloneDeep(this.state);
-            newState.location[prop] = newValue;
-            this.setState(newState);
-        },
-
-        valueLink: function (prop) {
-            //var value = prop === 'long' ? this.state.location.long : this.state.location.lat;
-            //var requestChange = prop === 'long' ? this.handleChangeLong : this.handleChangeLat;
-            return {
-                value: this.state.location[prop],
-                requestChange: this.handleChange.bind(this, prop)
-            };
+        getProps: function () {
+            return {style: {color: 'red'}};
         },
 
         render: function () {
